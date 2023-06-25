8e5e043fdee3b7d2b3e7a1ae2e4800eede286616
Shimbi Masengo Wa Umba Papa Levi <70257592+Lukushi@users.noreply.github.com>
2023-06-24 21:21:42 -0400

    Update preprocess.py

--- src/preprocess.py
+++ src/preprocess.py
@@ -12,13 +12,13 @@ def delete_unecessary_columns(df):
 def assign_and_range_pages(df):
     def assign_page_range(pages):
         if pages > 0 and pages <= 100:
-            return '[0-100]'
+            return '[0-100] pages'
         elif pages <= 250:
-            return '[101-250]'
+            return '[101-250] pages'
         elif pages <= 500:
-            return '[251-500]'
+            return '[251-500] pages'
         elif pages <= 2035:
-            return '[501-2035]'
+            return '[501-2035] pages'
         else: 
             return '0'
     
@@ -142,4 +142,3 @@ def rename_inclassable(df):
                        value='programme individualisé ou inconnu',
                        inplace=True)
     return df
-
