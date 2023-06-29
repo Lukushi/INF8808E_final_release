545ae36b90f2b1d334ad12c1f8b615f7654a8b87
Shimbi Masengo Wa Umba Papa Levi <70257592+Lukushi@users.noreply.github.com>
2023-06-29 19:25:02 -0400

    Update stacked_area_chart.py

--- src/stacked_area_chart.py
+++ src/stacked_area_chart.py
@@ -3,8 +3,15 @@ import pandas as pd
 from preprocess import other_languages, other_univ, rename_languages, rename_inclassable, get_top_univ
 from hover_template import get_hover_stacked_area_chart
 
-def get_figure(df, y_axis, mode):
 
+def get_figure(df, y_axis, mode):
+    '''
+    This function generates the stacked area figure, with its different y-axis and mode possibilities.
+    :param df: (pandas dataframe) data to visualize after basic pre-processing
+    :param y_axis: (str) y-axis to visualize, can be 'univ', 'domaine', 'langue' or 'grade'
+    :param mode: (str) mode of the chart, can be 'count' for counting the number of publications or 'percentage' for normalized values.
+    :return: (figure) the stacked area chart
+    '''
     titles = {'univ': 'Université',
               'domaine': 'Domaine',
               'langue': 'Langue',
@@ -58,20 +65,32 @@ def get_figure(df, y_axis, mode):
     fig.update_traces(hovertemplate=get_hover_stacked_area_chart(mode))
     fig.update_layout(legend_traceorder='reversed')
     fig.update_layout(height=600, width=1200)
-    fig.update_xaxes(title_text="Année", title_font=dict(size=20))
+    fig.update_xaxes(title_text="Année", title_font=dict(size=15))
+    fig.add_annotation(text=f'Evolution du nombre de publications par {titles[y_axis].lower()} en fonction du temps',
+                       # x = 0.95,
+                       y = 1.2,
+                       showarrow=False,
+                       font=dict(size=16),
+                       align='left',
+                       xref='paper',
+                       yref='paper')
+    fig.update_layout(margin=dict(t=100))
 
     if mode == 'count':
         fig.update_yaxes(title_text='')
         fig.update_layout(title=dict(text='Nombre de publications',
                                      xanchor='left',
                                      yanchor='top',
-                                     font=dict(size=20)))
+                                     font=dict(size=15),
+                                     y=0.88))
     elif mode == 'percentage':
         fig.update_yaxes(title_text='',
                          range=[0,100])
         fig.update_layout(title=dict(text='Pourcentage de publications [%]',
                                      xanchor='left',
                                      yanchor='top',
-                                     font=dict(size=20)))
+                                     font=dict(size=15),
+                                     y=0.88))
+    fig.update_traces(hoverlabel=dict(namelength=0))    
 
     return fig
